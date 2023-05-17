7119849e7b464ff7e9be8491e8ca1acc67226425
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-05-17 16:26:00 +0200

    add paths aliases config

---
+++ src/helpers/math.ts
@@ -0,0 +1,8 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export { add, multiply };
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 // tableau : Array<string>, string[]
 // object (pas assez précis)
 
+import { add, multiply } from "@helper/math";
+
 type stringOrUndefined = string | undefined;
 
 type Pokemon = {
@@ -15,12 +17,7 @@ let pokemon: Pokemon = {
   type: "Normal"
 }
 
-function add(a: number, b: number): number {
-  return a + b;
-}
-function multiply(a: number, b: number): number {
-  return a * b;
-}
+
 
 type callback = (value1: number, value2: number) => number
 
@@ -28,4 +25,5 @@ function calculator(a: number, b: number, action: callback): number {
   return action(a, b);
 }
 
+console.log(calculator(2, 3, add));
 console.log(calculator(2, 3, multiply));
